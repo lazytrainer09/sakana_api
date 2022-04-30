a9f59b0cdf0f218656e28e5f41e8752ef074b57f
lazytrainer09 <wihan0927@gmail.com>
2022-04-30 11:04:55 +0900

    Fix rubocop

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::Tasks', type: :request do
   describe 'GET /index' do
-    let!(:task1) { create(:task) }
-    let!(:task2) { create(:task) }
-    
-    before { get '/api/v1/tasks' }
-    
+    before do
+      create_list(:task, 10)
+      get '/api/v1/tasks'
+    end
+
     it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-    
+
     it 'returns all tasks' do
       json = JSON.parse(response.body)
       expect(Task.count).to eq(json.count)
