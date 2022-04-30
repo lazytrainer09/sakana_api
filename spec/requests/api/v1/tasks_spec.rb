a5b6e2f719d0f682b1753405acbda8ced5ea3a6a
lazytrainer09 <wihan0927@gmail.com>
2022-04-30 10:57:03 +0900

    Add request spec of Api::V1::TasksController#index

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Tasks", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/tasks/index"
+RSpec.describe 'Api::V1::Tasks', type: :request do
+  describe 'GET /index' do
+    let!(:task1) { create(:task) }
+    let!(:task2) { create(:task) }
+    
+    before { get '/api/v1/tasks' }
+    
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
+    
+    it 'returns all tasks' do
+      json = JSON.parse(response.body)
+      expect(Task.count).to eq(json.count)
+    end
   end
-
 end
